8650334d17eb5766bfe1da42127f460485a7a979
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-05-15 02:40:06 -0400

    Added a form page to let user make a POST request of submitting a new url to the database

--- express_server.js
+++ express_server.js
@@ -10,6 +10,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 /////////////////////////////////////////////////////////////////////////////////
 // Database
@@ -19,8 +20,6 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
-console.log('url data base: ', urlDatabase.b2xVn2);
 
 /////////////////////////////////////////////////////////////////////////////////
 // Routes
@@ -42,6 +41,20 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+/**
+ * Create a URL Page
+ * GET /urls/new
+ */
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 /**
